b154a7c01976d42f84794667624f1383278dc65d
Brian Turk <bturksub@gmail.com>
2019-03-05 04:06:16 -0800

    final version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,16 +18,9 @@ var tags = ["baseball", "football", "soccer", "basketball", "hockey", "trackandf
 var ratings = ["Y", "G", "PG", "PG-13"];
 var currentTag = "";
 var currentKey = 0;
-
-// var gifs = [{
-// 	id: "",
-// 	title: "",
-// 	image_gif: "",
-// 	image_still: "",
-// 	rating: "",
-// 	still: true
-// }]
-
+var totalGifs = 0;
+var newTag = "";
+var offset = 0;
 var gifs = [];
 
 $(document).ready(function () {
@@ -35,29 +28,175 @@ $(document).ready(function () {
 
 	//put tags on the page
 	showTags();
-	// currentTag = tags[currentKey];
-	// getGifs();
 
+
+	//Clicking "more gifs" text at the bottom of the list of gifs to get 10 more gifs
+	$(document).on("click","#more_gifs",function(){
+		offset = offset + 10;
+		$("#more_gifs").remove();
+		getGifs();
+	})
+
+
+	// When the user clicks on <span> (x), close the modal
+	$(document).on("click", ".close", function () {
+		$('#myModal').css("display", "none");
+	})
+
+	//User clicks the close button in the modal dialog box
+	$(document).on("click", ".new_tag_close", function () {
+		$('#myModal').css("display", "none");
+	})
+
+	//User clicks the ok button in the modal dialog box
+	$(document).on("click", ".new_tag_ok", function () {
+		newTag = $('#new_tag_enter').val();
+		$('#myModal').css("display", "none");
+
+		if (newTag != null && newTag != "") {
+
+			var dupe = false;
+			var x = 0;
+			//check to see if tag is a duplicate
+			while (!(dupe) && (x <= tags.length)) {
+				if (newTag === tags[x]) {
+					dupe = true;
+				}
+				x++;
+			}
+
+			if (dupe) { 		//found duplicate tag.  show alert and do not add
+				alert("You already have tag '" + newTag + "' in your list");
+			} else {			//add tag to list
+				tags.push(newTag);
+				currentKey = tags.length;
+				currentTag = newTag;
+				showTags();
+				gifs = [];
+				offset = 0;
+				$("#main").empty();
+				getGifs();
+			}
+
+			newTag = "";
+		}
+	})
+
+	//User clicks the ok in the delete tag modal box
+	$(document).on("click", ".delete_tag_ok", function () {
+
+		tags = jQuery.grep(tags, function (a) {
+			return a !== currentTag;
+		});
+
+		currentKey = 0;
+		currentTag = "";
+		showTags();
+		gifs = [];
+		offset = 0;
+		$("#main").empty();
+		getGifs();
+
+		$('#myModal').css("display", "none");
+	})
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function (event) {
+		if (event.target == $('#myModal')) {
+			$('#myModal').css("display", "none");
+		}
+	}
+
+	//Toggle between animated gif and still gif
 	$(document).on("click", ".gif_image", changeImage);
+
+	//Click a tag and show the gifs for that tag
 	$(document).on("click", ".tag", function () {
 
-		$("#tag_"+currentKey).removeClass("active");
+		$("#tag_" + currentKey).removeClass("active");
 		currentKey = $(this).attr("data-num");
-		$("#tag_"+currentKey).attr("class", "active");
+		$("#tag_" + currentKey).attr("class", "active");
 		gifs = [];
+		offset = 0;
 		currentTag = tags[currentKey];
 		$("#main").empty();
 		getGifs()
 
 	})
 
+	//Clicked tghe "Add a Tag" link
+	$(document).on("click", "#add_tag", function () {
 
+		$("#modal_header_text").text("Add a Tag")
+
+		var enterLabel = $("<label>");
+		enterLabel.attr("for", "newtag");
+		enterLabel.text("Enter your new tag");
+
+		var enterText = $("<input>")
+		enterText.attr("class", "form-control");
+		enterText.attr("type", "text");
+		enterText.attr("id", "new_tag_enter");
+
+		$(".modal-body").empty();
+		$(".modal-body").append(enterLabel, enterText);
+
+		var btnOK = $("<button>");
+		btnOK.attr("id", "#new_tag_ok");
+		btnOK.attr("class", "btn btn-secondary new_tag_ok");
+		btnOK.text("OK");
+
+		var btnCancel = $("<button>");
+		btnCancel.attr("id", "#new_tag_cancel");
+		btnCancel.attr("class", "btn btn-secondary new_tag_close");
+		btnCancel.text("CANCEL");
+
+		$(".modal-footer").empty();
+		$(".modal-footer").append(btnOK, btnCancel);
+
+
+		$('#myModal').css("display", "block");
+
+		enterText.focus();
+	})
+
+
+	//Clicked the delete tag icon in the upper right corner
+	$(document).on("click", "#delete_tag", function () {
+
+		$("#modal_header_text").text("Delete a Tag")
+
+		var deleteP = $("<P>");
+		deleteP.text("Do you want to delete '" + currentTag + "'?");
+
+		$(".modal-body").empty();
+		$(".modal-body").append(deleteP);
+
+		var btnOK = $("<button>");
+		btnOK.attr("id", "#new_tag_ok");
+		btnOK.attr("class", "btn btn-secondary delete_tag_ok");
+		btnOK.text("OK");
+
+		var btnCancel = $("<button>");
+		btnCancel.attr("id", "#new_tag_cancel");
+		btnCancel.attr("class", "btn btn-secondary new_tag_close");
+		btnCancel.text("CANCEL");
+
+		$(".modal-footer").empty();
+		$(".modal-footer").append(btnOK, btnCancel);
+
+
+		$('#myModal').css("display", "block");
+	})
+
+	//Display the list of tags
 	function showTags() {
 		//first sort the tags alphabetically
 		tags = tags.sort();
 		if (currentTag === "") {
 			currentTag = tags[0];
 			currentKey = 0;
+			offset = 0;
 			getGifs();
 		}
 
@@ -65,7 +204,7 @@ $(document).ready(function () {
 
 		$.each(tags, function (key, value) {
 			var tLi = $("<li>");
-			tLi.attr("id","tag_"+key);
+			tLi.attr("id", "tag_" + key);
 			if (value === currentTag) {
 				tLi.attr("class", "active");
 			}
@@ -80,20 +219,11 @@ $(document).ready(function () {
 
 			$("#tags").append(tLi);
 		})
-
-		//clear tag list
-		// <li class="sidebar-brand"> <a href="#"> Add a Tag </a> </li>
-
-
-		//add tags to list
-
 	}
 
+	//Swap between still and animated image
 	function changeImage() {
 		gifKey = $(this).parent().attr("data-num");
-
-		// console.log(gifKey);
-		// console.log(gifs);
 
 		if (gifs[gifKey].still) {  //toggle image and gif
 			$(this).attr("src", gifs[gifKey].image_gif);
@@ -106,37 +236,34 @@ $(document).ready(function () {
 
 
 	function getGifs() {
-		// var apiKey = "7mqpXLfwgxU0ELM4LULox0Aa04gKwkA5"
-		// var apiKey = "vkVcwtm6Qn8d7x8VbAj8IKB5fNQldtxR"
+
+		//update current tag on the screen
+		$("#current_tag").text(currentTag);
 		var apiKey = "FYH24Rrm6xWi8PN1FbMMhGpJK1WivHiM"
+		var apiURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + currentTag + "&offset=" + offset + "&limit=10&rating=PG-13&lang=en"
 
-		var apiURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + currentTag + "&limit=10&rating=PG-13&lang=en"
-
-		var x = 0;
-
-		
-		// console.log(apiURL);
 		$.ajax({
 			url: apiURL,
 			method: "GET"
 		}).then(function (response) {
 
-			// console.log(response);
+			totalGifs = response.pagination.total_count;
 			addGifs(response.data);
-
 		});
 
+		//Add gifs to the page
 		function addGifs(jsonGifs) {
 
 			$.each(jsonGifs, function (key, value) {
 				var gDiv = $("<div>");
 				gDiv.attr("class", "gif_card");
 				gDiv.attr("data-id", value.id);
-				gDiv.attr("data-num", key);
+				gDiv.attr("data-num", key + offset);
 
 				var gImg = $("<img>");
 				gImg.attr("class", "gif_image");
 				gImg.attr("src", value.images.original_still.url);
+				gImg.attr("alt", value.title);
 
 
 				var gDivOF = $("<div>");
@@ -165,6 +292,24 @@ $(document).ready(function () {
 
 			})
 
+			if (gifs.length === 0) {		//no gifs found, display a message instead
+				var ngP = "<p>";
+				ng.text("No Gifs found for that tag.");
+
+				$("#main").append(ngP);
+			} else if (gifs.length < totalGifs) {  //Gifs left in the API then show a "more gifs" option
+
+				var moreDiv = $("<div>");
+				moreDiv.attr("id","more_gifs")
+				var moreP = $("<p>");
+
+				moreP.text("More Gifs...");
+
+				moreDiv.append(moreP);
+
+				$("#main").append(moreDiv);
+
+			}
 		}
 	}
 
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Giftastic
+Brian Turk  
+3/5/19
+
+This is the Giftastic application.  Programming notes:
+* It is fully responsive.  I used a template for the sidebar, but made modifications to it to get it to look the way I wanted
+* You can get more gifs by clicking the more gifs button at the bottom of the page
+
+
+Note that the Portfolio page of my Responsive Portfolio project has been updated with this project.
